93eb4b9863accad513879b6c83bad56b5f2ee38d
lordvirgin <891276009a@gmail.com>
2024-12-23 20:42:07 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Андрей"
+title = "grade"
+content = "study"
+status = "active"
+created_date = "23-12-2024"
+issue_date = "26-12-2024"
+
+print("Имя пользователя", username)
+print("Заголовок", title)
+print("Содержание", content)
+print("Статус", status)
+print("Дата создания", created_date)
+print("Дата выполнения", issue_date)
