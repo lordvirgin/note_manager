f18240bc0b06bfb31daff5c27a5d89390eb86907
lordvirgin <891276009a@gmail.com>
2024-12-24 14:25:50 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,9 @@
+take_note = input("Напишите заметку:" )
+title = input("Напишите заголовок:" )
+title1 = input("Напишите заголовок:" )
+title2 = input("Напишите заголовок:" )
+list = [title, title1, title2]
+print(take_note)
+print(list[0])
+print(list[1])
+print(list[2])
